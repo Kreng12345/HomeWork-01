b7bdf8092a38bb4880a79399efe47057bee15bdc
73dave <73dave@mail.ru>
2023-09-28 05:30:47 +0300

    Задача 08 выполнена

---
+++ HomeTask 04(08)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число num = ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 3)
+{
+    Console.Write($"Перед числом {num} нет четных чисел");
+}
+int count = 2;
+while (count <= num)
+{
+    Console.Write($"{count} ");
+    count = count + 2;
+}
